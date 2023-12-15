7d30ccdbd3e0afe55defeff1d8c9bc662d1dbf79
resha06604 <112202106604@mhs.dinus.ac.id>
2023-12-15 14:06:05 +0700

    krm tugas

--- krm.php
+++ krm.php
@@ -2,14 +2,16 @@
 require "fungsi.php";
 $npp = $_GET['npp'];
 $sql = "select * from kultawar a join matkul b on (a.idmatkul=b.id) where a.npp = '" . $npp . "'";
+
 $rs = mysqli_query($koneksi, $sql) or die(mysqli_error($koneksi));
-$mhs = search("dosen", "npp='" . $npp . "'", $npp);
+$dosen = search("dosen", "npp='" . $npp . "'", $npp);
+
 $sks = 0;
-$rsmhs = mysqli_fetch_assoc($mhs);
+$rsdosen = mysqli_fetch_assoc($dosen);
 $html = "<div style= 'width:100%; text-align:center'><h3>KRM Dosen</h3></div>";
-$html .= "<p>NPP : " . $rsmhs["npp"] . "</p>";
-$html .= "<p>Nama : " . $rsmhs["namadosen"] . "</p>";
-$html .= "<table style='border:1px solid black; border-collapse: collapse'>
+$html .= "<p>NPP : " . $rsdosen["npp"] . "</p>";
+$html .= "<p>Nama : " . $rsdosen["namadosen"] . "</p>";
+$html .= "<table style='width:100%; border:1px solid black; border-collapse: collapse'>
     <thead class='thead-light'>
     <tr style='border:1px solid black;'>
         <th style='border:1px solid black;'>No</th>
@@ -26,10 +28,10 @@ while ($row = mysqli_fetch_assoc($rs)) {
     $sks += $row['sks'];
     $html .= "
     <tr style='border:1px solid black;'>
-        <td style='border:1px solid black;'>" . $i . "</td>
-        <td style='border:1px solid black;'>" . $row['idmatkul'] . "</td>
-        <td style='border:1px solid black;'>" . $row['namamatkul'] . "</td>
-        <td style='border:1px solid black;'>" . $row['klp'] . "</td>
+        <td style='text-align: center;border:1px solid black;'>" . $i . "</td>
+        <td style='text-align: center;border:1px solid black;'>" . $row['idmatkul'] . "</td>
+        <td style='text-align: center;border:1px solid black;'>" . $row['namamatkul'] . "</td>
+        <td style='text-align: center;border:1px solid black;'>" . $row['klp'] . "</td>
         <td style='border:1px solid black;text-align:center'>" . $row['sks'] . "</td>
         <td style='text-align: center; border:1px solid black;'>" . $row['hari'] . " - " . $row['jamkul'] . "</td>
     </tr>";

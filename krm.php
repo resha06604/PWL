8528ffa3ebd1c32134b1ac65b9d672fc79a4aac9
resha06604 <112202106604@mhs.dinus.ac.id>
2023-12-15 09:38:16 +0700

    tez

--- krm.php
+++ krm.php
@@ -13,10 +13,13 @@ $html .= "<table style='border:1px solid black; border-collapse: collapse'>
     <thead class='thead-light'>
     <tr style='border:1px solid black;'>
         <th style='border:1px solid black;'>No</th>
-        <th style='border:1px solid black;'>SKS</th>
+        <th style='border:1px solid black;'>Kode</th>
+        <th style='text-align: center; border:1px solid black;'>Nama Mata Kuliah</th>
+        <th style='text-align: center; border:1px solid black;'>Kelompok</th>
+        <th style='text-align: center; border:1px solid black;'>SKS</th>
         <th style='text-align: center; border:1px solid black;'>Jadwal</th>
-        <th style='text-align: center; border:1px solid black;'>zzzz</th>
-        <th style='text-align: center; border:1px solid black;'>Jadwal</th>
+        <th style='text-align: center; border:1px solid black;'>Ruang</th>
+
     </tr>
 </thead>";
 $i = 1;
@@ -25,8 +28,9 @@ while ($row = mysqli_fetch_assoc($rs)) {
     $html .= "
     <tr style='border:1px solid black;'>
         <td style='border:1px solid black;'>" . $i . "</td>
-        <td style='border:1px solid black;'>" . $row['sks'] . "</td>
+        <td style='border:1px solid black;'>" . $row['idmatkul'] . "</td>
         <td style='border:1px solid black;'>" . $row['namamatkul'] . "</td>
+        <td style='border:1px solid black;'>" . $row['klp'] . "</td>
         <td style='border:1px solid black;'>" . $row['sks'] . "</td>
         <td style='text-align: center; border:1px solid black;'>" . $row['hari'] . " - " . $row['jamkul'] . "</td>
     </tr>";
